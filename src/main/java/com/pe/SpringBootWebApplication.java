6030fbcb308663c27043ef7ed7c81d9b3c0fefd9
MARCOZAVALETA <MARCOZAVALETA@PETRELLI>
2019-03-09 15:29:26 -0500

    1

---
+++ src/main/java/com/pe/SpringBootWebApplication.java
@@ -0,0 +1,21 @@
+package com.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebApplication.class);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+    
+}
